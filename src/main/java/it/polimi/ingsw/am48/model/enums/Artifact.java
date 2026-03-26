f0dbb13cb97c5234ebab7ecc1bc0dec0df285d4a
Simone Clerico <simone.clerico@mail.polimi.it>
2026-03-26 15:29:54 +0100

    Feat: add all enums (Era, Totem, CharacterType, EventType, Resource, Artifact)

---
+++ src/main/java/it/polimi/ingsw/am48/model/enums/Artifact.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.am48.model.enums;
+
+public enum Artifact {
+    ARROW,
+    BREAD,
+    BOWL,
+    CANOE,
+    DOLL,
+    FLUTE,
+    HOOK,
+    LEATHER,
+    NECKLACE,
+    ROPE
+}
