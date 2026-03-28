21272146dae62e02961d4b9f10b1d810d59d36ae
alessiocasati <alessiocasati.uni@gmail.com>
2026-03-28 19:43:13 +0100

    feat: implement OfferTurnCard.setupOrder with shuffle

--- src/main/java/it/polimi/ingsw/am48/model/board/OfferTurnCard.java
+++ src/main/java/it/polimi/ingsw/am48/model/board/OfferTurnCard.java
@@ -1,23 +1,29 @@
 package it.polimi.ingsw.am48.model.board;
 
+import it.polimi.ingsw.am48.model.enums.Resource;
 import it.polimi.ingsw.am48.model.player.Player;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class OfferTurnCard {
     private final int numPlayers;
     private final List<Player> positions;
-    private final CardStrategy effect;
+    private final List<Integer> foodRewards;
 
-    public OfferTurnCard(int numPlayers, CardStrategy effect){
+    public OfferTurnCard(int numPlayers, List<Integer> foodRewards) {
         this.numPlayers = numPlayers;
+        this.foodRewards = List.copyOf(foodRewards);
         this.positions = new ArrayList<>();
-        this.effect = effect;
     }
 
-    // metodi della OfferTurnCard ???
+    // randomizes player order for the very first turn
+    public void setupOrder(List<Player> players){
+        this.positions.addAll(players);
+        Collections.shuffle(this.positions);
+    }
 
     // getSnapshot
 }
