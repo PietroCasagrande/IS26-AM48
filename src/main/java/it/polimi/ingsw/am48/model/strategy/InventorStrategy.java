455a7b1a75eaca936a01f17bf7160a4cf58b4bc9
Simone Clerico <simone.clerico@mail.polimi.it>
2026-03-30 00:51:32 +0200

    feat: implement InventorStrategy and its tests

---
+++ src/main/java/it/polimi/ingsw/am48/model/strategy/InventorStrategy.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.am48.model.strategy;
+
+import it.polimi.ingsw.am48.model.enums.Artifact;
+import it.polimi.ingsw.am48.model.player.Player;
+
+import java.util.List;
+
+public class InventorStrategy extends CardStrategy{
+
+    private final Artifact artifact;
+
+    public InventorStrategy(RegistrationAction registrationAction, Artifact artifact) {
+        super(registrationAction);
+        this.artifact = artifact;
+    }
+
+    @Override
+    public void effect(Player player, List<Player> allPlayers) {
+        player.updateArtifacts(artifact);
+    }
+}
