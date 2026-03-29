740033412e038fad3c904676290419cd7902b8a5
Luca M. Brotto <luca@brotto.org>
2026-03-29 18:02:46 +0200

    feat: implement OnPickNotificator.attach

--- src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
+++ src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.am48.model.notificator;
 import it.polimi.ingsw.am48.model.player.Player;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +11,12 @@ public class OnPickNotificator {
     private Map<Player, List<CardStrategy>> listeners;
 
     public void attach(Player p, CardStrategy cs){
-        throw new UnsupportedOperationException("TODO");    }
+        listeners.computeIfAbsent(p, k -> new ArrayList<>())
+                .add(cs);
+    }
+
+
+
 
     public void detach(){
         throw new UnsupportedOperationException("TODO");    }
