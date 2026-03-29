8797be4e519143fc0f60c157cbc7d9c78523dd22
alessiocasati <alessiocasati.uni@gmail.com>
2026-03-29 17:16:11 +0200

    test: add test for OfferCard with 100% coverage

--- src/main/java/it/polimi/ingsw/am48/model/board/OfferCard.java
+++ src/main/java/it/polimi/ingsw/am48/model/board/OfferCard.java
@@ -10,32 +10,34 @@ import java.util.Optional;
 public class OfferCard {
     private final char letterId;                // Letters from A to G, depending on number of players
     private final CardStrategy strategy;
+    private final int numPlayers;
     private Player totem;
 
-    public OfferCard(char letterId, CardStrategy strategy){
+    public OfferCard(char letterId, CardStrategy strategy, int numPlayers) {
         this.letterId = letterId;
         this.strategy = strategy;
+        this.numPlayers = numPlayers;
         this.totem = null;
     }
 
-    // places player totem to compute offer phase
+    // Places player totem to compute offer phase
     public void placeTotem(Player player){
         if(this.totem != null){ throw new IllegalStateException("Cannot place totem: this position is already occupied"); }
         this.totem = player;
     }
 
-    // returns totem after offer phase
+    // Returns totem after offer phase
     public Optional<Player> returnTotem(){
-        Optional<Player> removedTotem = Optional.of(this.totem);
+        Optional<Player> removedTotem = Optional.ofNullable(this.totem);
         this.totem = null;
         return removedTotem;
     }
 
+    /*
+    // To implement: waiting for PickCardStrategy definition
     public Card activateStrategy(Board board, String cardId, Player player, GamePhase phase){
         throw new UnsupportedOperationException("TODO");
-    }
+    } */
 
     // getSnapshot()
-
-    // removeTotem()
 }
