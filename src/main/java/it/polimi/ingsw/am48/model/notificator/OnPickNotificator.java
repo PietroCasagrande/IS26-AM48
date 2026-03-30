505ef8ca2e40ad2d4f6ef1e27ea750ac43b64621
Luca M. Brotto <luca@brotto.org>
2026-03-30 09:10:52 +0200

    feat: implement OnPickNotificator.detach

--- src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
+++ src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.am48.model.notificator;
 
+import it.polimi.ingsw.am48.exception.StrategyNotFoundException;
 import it.polimi.ingsw.am48.model.player.Player;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
@@ -15,11 +16,12 @@ public class OnPickNotificator {
                 .add(cs);
     }
 
-
-
-
-    public void detach(){
-        throw new UnsupportedOperationException("TODO");    }
+    public void detach(Player p, CardStrategy cs){
+        if (listeners.get(p) == null || !listeners.get(p).contains(cs)) {
+            throw new StrategyNotFoundException("Impossibile rimuovere la strategia: " + cs + " per il giocatore " + p);
+        }
+        cs.unregister(this,p);
+    }
 
     public void notifyListeners(Player p){
         throw new UnsupportedOperationException("TODO");    }
