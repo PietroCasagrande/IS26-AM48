2664a55ba02cb463b072d917137b94789e006b88
Luca M. Brotto <luca@brotto.org>
2026-03-30 18:18:18 +0200

    feat: implement OnPickNotificator.notify

--- src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
+++ src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.am48.model.notificator;
 
 import it.polimi.ingsw.am48.exception.StrategyNotFoundException;
 import it.polimi.ingsw.am48.model.player.Player;
+import it.polimi.ingsw.am48.model.player.PlayerContext;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
 import java.util.ArrayList;
@@ -17,12 +18,28 @@ public class OnPickNotificator {
     }
 
     public void detach(Player p, CardStrategy cs){
-        if (listeners.get(p) == null || !listeners.get(p).contains(cs)) {
+        List<CardStrategy> strategies = listeners.get(p);
+
+        if (strategies == null || !strategies.contains(cs)) {
             throw new StrategyNotFoundException("Impossibile rimuovere la strategia: " + cs + " per il giocatore " + p);
         }
-        cs.unregister(this,p);
+        strategies.remove(cs);
+        if (strategies.isEmpty()) listeners.remove(p);
     }
 
-    public void notifyListeners(Player p){
-        throw new UnsupportedOperationException("TODO");    }
+    public void notify(PlayerContext p) {
+        List<CardStrategy> strategies = listeners.get(p);
+        if (strategies == null) return;
+
+        List<CardStrategy> toDetach = new ArrayList<>();
+
+        for (CardStrategy cs : strategies) {
+            cs.effect(p);
+            cs.unregisterFrom(toDetach); // the strategy may add itself to the array if its effect works once
+        }
+
+        // removing out of the cycle to avoid problems
+        strategies.removeAll(toDetach);
+        if (strategies.isEmpty()) listeners.remove(p);
+    }
 }
