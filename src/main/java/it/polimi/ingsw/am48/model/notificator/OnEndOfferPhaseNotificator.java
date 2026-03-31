df8229bae0bf6875850f7c5252f279c82eb5a3db
Luca M. Brotto <luca@brotto.org>
2026-03-31 15:35:34 +0200

    feat: implement OnEndOfferPhaseNotificator.attach

--- src/main/java/it/polimi/ingsw/am48/model/notificator/OnEndOfferPhaseNotificator.java
+++ src/main/java/it/polimi/ingsw/am48/model/notificator/OnEndOfferPhaseNotificator.java
@@ -3,14 +3,15 @@ package it.polimi.ingsw.am48.model.notificator;
 import it.polimi.ingsw.am48.model.player.Player;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class OnEndOfferPhaseNotificator {
-    private Map<Player, CardStrategy> listeners;
+    private Map<Player, CardStrategy> listeners = new HashMap<>();
 
-    public void attach(Player p, CardStrategy cs){
-        throw new UnsupportedOperationException("TODO");    }
+    public void attach(Player p, CardStrategy cs) {
+        listeners.put(p, cs);
+    }
 
     public void notifyListeners(){
         throw new UnsupportedOperationException("TODO");    }
