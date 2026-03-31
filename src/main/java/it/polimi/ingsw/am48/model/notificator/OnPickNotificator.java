ebe4ff99f7e5a7c872e3e1ef1772cf456cb42520
Luca M. Brotto <luca@brotto.org>
2026-03-31 08:41:20 +0200

    test: add tests for OnPickNotificator with 100% coverage

--- src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
+++ src/main/java/it/polimi/ingsw/am48/model/notificator/OnPickNotificator.java
@@ -6,12 +6,13 @@ import it.polimi.ingsw.am48.model.player.PlayerContext;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class OnPickNotificator {
-    private Map<Player, List<CardStrategy>> listeners;
-
+    // private Map<Player, List<CardStrategy>> listeners;
+    private Map<Player, List<CardStrategy>> listeners = new HashMap<>();
     public void attach(Player p, CardStrategy cs){
         listeners.computeIfAbsent(p, k -> new ArrayList<>())
                 .add(cs);
@@ -28,7 +29,7 @@ public class OnPickNotificator {
     }
 
     public void notify(PlayerContext p) {
-        List<CardStrategy> strategies = listeners.get(p);
+        List<CardStrategy> strategies = listeners.get(p.getCurrPlayer());
         if (strategies == null) return;
 
         List<CardStrategy> toDetach = new ArrayList<>();
@@ -40,6 +41,6 @@ public class OnPickNotificator {
 
         // removing out of the cycle to avoid problems
         strategies.removeAll(toDetach);
-        if (strategies.isEmpty()) listeners.remove(p);
+        if (strategies.isEmpty()) listeners.remove(p.getCurrPlayer());
     }
 }
