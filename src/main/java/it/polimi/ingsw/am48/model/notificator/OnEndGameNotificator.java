8c93c54aed36cdaeea693f969a269ffcf386aeb1
Luca M. Brotto <luca@brotto.org>
2026-03-31 19:40:00 +0200

    feat: implement OnEndGameNotificator.attach

--- src/main/java/it/polimi/ingsw/am48/model/notificator/OnEndGameNotificator.java
+++ src/main/java/it/polimi/ingsw/am48/model/notificator/OnEndGameNotificator.java
@@ -3,16 +3,18 @@ package it.polimi.ingsw.am48.model.notificator;
 import it.polimi.ingsw.am48.model.player.Player;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class OnEndGameNotificator {
-    private Map<Player, List<CardStrategy>> listeners;
+    private Map<Player, List<CardStrategy>> listeners = new HashMap<>();
 
     public void attach(Player p, CardStrategy cs){
-        throw new UnsupportedOperationException("TODO");
+        listeners.computeIfAbsent(p, k -> new ArrayList<>())
+                .add(cs);
     }
-
     public void notifyListeners(){
         throw new UnsupportedOperationException("TODO");
     }
