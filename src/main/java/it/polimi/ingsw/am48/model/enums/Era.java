1db293414de9cfd8c110df335b55aa62416f693f
alessiocasati <alessiocasati.uni@gmail.com>
2026-03-31 16:00:45 +0200

    feat: implement Board class and modified Era enum with index and next

--- src/main/java/it/polimi/ingsw/am48/model/enums/Era.java
+++ src/main/java/it/polimi/ingsw/am48/model/enums/Era.java
@@ -1,7 +1,17 @@
 package it.polimi.ingsw.am48.model.enums;
 
 public enum Era {
-    FIRST,
-    SECOND,
-    THIRD
+    FIRST(0),
+    SECOND(1),
+    THIRD(2);
+
+    private final int index;
+
+    Era(int index) { this.index = index; }
+    public int getIndex() { return index; }
+
+    public Era next() {
+        if (this == THIRD) return THIRD;
+        return values()[this.ordinal() + 1];
+    }
 }
