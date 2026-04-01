beaa3b9a1b344190923971cbef4213734a7e2d53
Simone Clerico <simone.clerico@mail.polimi.it>
2026-04-01 17:08:19 +0200

    feat: add building 11 effect to UpdateResourcesStrategy and add shamanSafety logic in Player and Tribe

---
+++ src/main/java/it/polimi/ingsw/am48/model/strategy/ShamanSafetyStrategy.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.am48.model.strategy;
+
+import it.polimi.ingsw.am48.model.player.PlayerContext;
+
+public class ShamanSafetyStrategy extends CardStrategy {
+
+    protected ShamanSafetyStrategy(RegistrationAction registration) {
+        super(registration);
+    }
+
+    @Override
+    public void effect(PlayerContext playerContext) {
+
+    }
+}
