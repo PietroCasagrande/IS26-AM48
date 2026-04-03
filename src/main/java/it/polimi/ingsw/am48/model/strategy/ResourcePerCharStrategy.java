fd2f99e075db3f386454fe38804a194df0772597
Simone Clerico <simone.clerico@mail.polimi.it>
2026-04-03 14:26:42 +0200

    feat: implement HuntEventStrategy and add FOOD_POINTS in Resource enum

--- src/main/java/it/polimi/ingsw/am48/model/strategy/ResourcePerCharStrategy.java
+++ src/main/java/it/polimi/ingsw/am48/model/strategy/ResourcePerCharStrategy.java
@@ -19,11 +19,16 @@ public class ResourcePerCharStrategy extends CardStrategy{
 
     @Override
     public void effect(PlayerContext playerContext) {
-        int amount = quantity * playerContext.getCurrPlayer().getTribe().countByType(characterType);
+        int num = playerContext.getCurrPlayer().getTribe().countByType(characterType);
+        int amount = quantity * num;
         switch (resource) {
-            case FOOD -> playerContext.getCurrPlayer().updateFood(amount);
-            case FOOD_DISCOUNT ->  playerContext.getCurrPlayer().updateFoodDiscount(amount);
-            case PRESTIGE_POINT -> playerContext.getCurrPlayer().updatePoints(amount);
+            case FOOD -> playerContext.getCurrPlayer().updateFood(amount);    // hunter character
+            case FOOD_POINTS -> {    // edificio 7 (1 food e 1 pp per hunter) e forse chiamato da HuntEvent
+                playerContext.getCurrPlayer().updateFood(num);    // food aggiornato è sempre +1
+                playerContext.getCurrPlayer().updatePoints(amount);    // pp aggiornati sono 1 per edificio 7 ma 1-3 per evento caccia (in caso si voglia chiamare questa strategy iterativamente dentro HuntEvent)
+            }
+            case FOOD_DISCOUNT ->  playerContext.getCurrPlayer().updateFoodDiscount(amount);    // edifici sconto carestia
+            case PRESTIGE_POINT -> playerContext.getCurrPlayer().updatePoints(amount);    // edifici pp a fine game
             default -> throw new IllegalArgumentException("Invalid resource " + resource);
         }
     }
