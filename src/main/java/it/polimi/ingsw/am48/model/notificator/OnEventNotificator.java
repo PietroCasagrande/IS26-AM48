e4f817c6512e22c48003cabf7530fa1cc46855a6
Luca M. Brotto <luca@brotto.org>
2026-04-04 18:50:42 +0200

    feat: implement OnEventNotificator.attach

--- src/main/java/it/polimi/ingsw/am48/model/notificator/OnEventNotificator.java
+++ src/main/java/it/polimi/ingsw/am48/model/notificator/OnEventNotificator.java
@@ -1,16 +1,22 @@
 package it.polimi.ingsw.am48.model.notificator;
 
 import it.polimi.ingsw.am48.model.enums.EventType;
+import it.polimi.ingsw.am48.model.player.Player;
 import it.polimi.ingsw.am48.model.strategy.CardStrategy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class OnEventNotificator {
-    private Map<EventType, List<CardStrategy>> listeners;
+    private Map<EventType, Map<Player,List<CardStrategy>>> listeners = new HashMap<>();;
 
-    public void attach(EventType e, CardStrategy cs){
-        throw new UnsupportedOperationException("TODO");    }
+    public void attach(EventType e, Player p, CardStrategy cs) {
+        listeners.computeIfAbsent(e, k -> new HashMap<>())
+                .computeIfAbsent(p, k -> new ArrayList<>())
+                .add(cs);
+    }
 
     public void detach(){
         throw new UnsupportedOperationException("TODO");    }
