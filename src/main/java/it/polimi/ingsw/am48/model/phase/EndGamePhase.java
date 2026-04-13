40c40e2b300c079db99a30eff316a0f63f0f260f
Pietro Casagrande <pitcasa04.pc@gmail.com>
2026-04-13 15:34:59 +0200

    feat: implemented resolveEndGame() method in EndGamePhase
    end game notificator, final score compute and game's winner

--- src/main/java/it/polimi/ingsw/am48/model/phase/EndGamePhase.java
+++ src/main/java/it/polimi/ingsw/am48/model/phase/EndGamePhase.java
@@ -1,14 +1,35 @@
 package it.polimi.ingsw.am48.model.phase;
 
+import it.polimi.ingsw.am48.model.delta.EndGameDelta;
 import it.polimi.ingsw.am48.model.delta.GameDelta;
 import it.polimi.ingsw.am48.model.game.Game;
+import it.polimi.ingsw.am48.model.player.Player;
 import it.polimi.ingsw.am48.model.snapshot.EndGamePhaseSnapshot;
 import it.polimi.ingsw.am48.model.snapshot.PhaseSnapshot;
 
-public class EndGamePhase implements GamePhase {
-    public GameDelta resolveEndGame(){ throw new UnsupportedOperationException("TODO"); }
+import java.util.HashMap;
+import java.util.Map;
 
-    // public void onEndGameNotify(Game game){ throw new UnsupportedOperationException("TODO");}
+public class EndGamePhase implements GamePhase {
+    public GameDelta resolveEndGame(Game game){
+        // activating end-game strategies
+        game.getNotificatorCenter().getEndGameNotificator().notify(game.getPlayerContext());
+
+        // computing end-game points for each player
+        game.getPlayerContext().getPlayers()
+                .forEach(p -> p.getTribe().computeTotalEndGameScore());
+
+        // building the final scores map
+        Map<String, Integer> finalScores = new HashMap<>();
+        game.getPlayerContext().getPlayers()
+                .forEach(p -> finalScores.put(p.getNickname(), p.getPoints()));
+
+        // finding game's winner
+        Player winner = game.findWinner();
+
+        // returns the Delta containing the final score of each player, and winner's nickname
+        return new EndGameDelta(finalScores, winner.getNickname());
+    }
 
     @Override
     public PhaseSnapshot toSnapshot(){
