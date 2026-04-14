9f1139bd4638ff8232de99af1c68f1d1a88a00cc
Simone Clerico <simone.clerico@mail.polimi.it>
2026-04-14 09:36:07 +0200

    fix: remove addTotem()
    returnTotem does its work

--- src/main/java/it/polimi/ingsw/am48/model/board/OfferTurnCard.java
+++ src/main/java/it/polimi/ingsw/am48/model/board/OfferTurnCard.java
@@ -34,11 +34,6 @@ public class OfferTurnCard {
         }
     }
 
-    // add totem to its order list
-    public void addTotem(Player player){
-        this.order.add(player);
-    }
-
     // Removes the totem to be placed on the offer track
     public void removeNextTotem(){
         this.order.removeFirst();
