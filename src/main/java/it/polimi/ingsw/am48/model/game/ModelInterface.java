a841b95ca8d92980fd9122e2b8cfa2db61b53a03
Luca M. Brotto <luca@brotto.org>
2026-04-16 00:49:16 +0200

    refactor: implement and adapt GameManager and GameController logic to network protocol
    # Conflicts:
    #	src/main/java/it/polimi/ingsw/am48/model/game/GameManager.java

--- src/main/java/it/polimi/ingsw/am48/model/game/ModelInterface.java
+++ src/main/java/it/polimi/ingsw/am48/model/game/ModelInterface.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.am48.model.game;
 
 import it.polimi.ingsw.am48.model.delta.GameDelta;
+import it.polimi.ingsw.am48.model.snapshot.GameSnapshot;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface ModelInterface {
     // metodi in-game: azioni del giocatore
     GameDelta placeTotem(String nickname, char position);
     List<GameDelta> takeCard(String name, String cardId);
+
+    GameSnapshot getSnapshotForNickname(String nickname);
+    boolean isGameFull(String nickname);
 }
