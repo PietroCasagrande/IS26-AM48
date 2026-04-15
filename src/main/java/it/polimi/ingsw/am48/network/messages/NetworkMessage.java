ec15aed151f8118dcf16783f154e9681c46f635f
Luca M. Brotto <luca@brotto.org>
2026-04-16 00:52:01 +0200

    feat: implement NetworkMessage class

---
+++ src/main/java/it/polimi/ingsw/am48/network/messages/NetworkMessage.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.am48.network.messages;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class NetworkMessage {
+    private String type; // es: "joinGame", "gameDelta", "error"
+    private JsonNode payload; // Il contenuto dinamico
+
+    // costruttori, getter e setter per Jackson
+    public NetworkMessage() {}
+
+    public NetworkMessage(String type, JsonNode payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public JsonNode getPayload() { return payload; }
+    public void setPayload(JsonNode payload) { this.payload = payload; }
+}
