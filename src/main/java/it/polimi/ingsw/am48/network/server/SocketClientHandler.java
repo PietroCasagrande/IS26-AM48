971627e97980c1af4b805bc33633089c6d9403e3
Luca M. Brotto <luca@brotto.org>
2026-04-16 00:59:25 +0200

    feat: implement MesosServer, ServerMain and SocketClientHandler class

---
+++ src/main/java/it/polimi/ingsw/am48/network/server/SocketClientHandler.java
@@ -0,0 +1,118 @@
+package it.polimi.ingsw.am48.network.server;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.polimi.ingsw.am48.controller.GameController;
+import it.polimi.ingsw.am48.model.delta.GameDelta;
+import it.polimi.ingsw.am48.model.snapshot.GameSnapshot;
+import it.polimi.ingsw.am48.network.VirtualViewSocket;
+import it.polimi.ingsw.am48.network.messages.NetworkMessage;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.List;
+
+public class SocketClientHandler implements Runnable, VirtualViewSocket {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private ObjectMapper mapper;
+    private GameController controller;
+    private MesosServer server; // Riferimento al server principale per il broadcast
+    private String nickname; // Identifica questo client
+
+    public SocketClientHandler(Socket socket, GameController controller, MesosServer server) throws IOException {
+        this.socket = socket;
+        this.controller = controller;
+        this.server = server;
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.mapper = new ObjectMapper();
+    }
+
+    @Override
+    public void run() {
+        try {
+            String line;
+            while ((line = in.readLine()) != null) {
+                NetworkMessage msg = mapper.readValue(line, NetworkMessage.class);
+                handleMessage(msg);
+            }
+        } catch (IOException e) {
+            System.err.println("connessione persa con " + nickname);
+
+            // da gestire disconnessione
+
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleMessage(NetworkMessage msg) {
+        try {
+            switch (msg.getType()) {
+                case "joinGame":
+                    int numPlayers = msg.getPayload().get("numPlayers").asInt();
+                    this.nickname = msg.getPayload().get("nickname").asText();
+                    server.registerClient(nickname, this); // Registra la view
+
+                    GameSnapshot snap = controller.handleJoinGame(numPlayers, nickname);
+                    if (controller.isGameFull(nickname)) {
+                        // Scenario join game e set up game (1B): broadcast a tutti i player della partita
+                        server.broadcastSnapshotToGame(nickname, snap);
+                    } else {
+                        // Scenario snapshot incompleto (1A): solo a questo client
+                        showInitialSnapshot(snap);
+                    }
+                    break;
+                case "placeTotem":
+                    char pos = msg.getPayload().get("position").asText().charAt(0);
+                    GameDelta delta = controller.handlePlaceTotem(this.nickname, pos);
+                    server.broadcastToGame(this.nickname, delta);
+                    break;
+                case "takeCard":
+                    String cardId = msg.getPayload().get("cardId").asText();
+                    List<GameDelta> deltas = controller.handleTakeCard(this.nickname, cardId);
+                    for (GameDelta d : deltas) {
+                        server.broadcastToGame(this.nickname, d);
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            try {
+                reportError(e.getMessage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    // --- Implementazione VirtualViewSocket (invia messaggi AL client) ---
+
+    @Override
+    public void showGameDelta(GameDelta delta) {
+        sendMessage("gameDelta", delta);
+    }
+
+    @Override
+    public void showInitialSnapshot(GameSnapshot snapshot) {
+        sendMessage("initialSnapshot", snapshot);
+    }
+
+    @Override
+    public void reportError(String errorMessage) {
+        sendMessage("error", errorMessage); // Crea un piccolo record/oggetto per l'errore se serve
+    }
+
+    private void sendMessage(String type, Object payloadData) {
+        try {
+            NetworkMessage msg = new NetworkMessage(type, mapper.valueToTree(payloadData));
+            out.println(mapper.writeValueAsString(msg));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
