8b9abc6e423a8cb456e612bf70da629fdacca0cc
Pietro Casagrande <pitcasa04.pc@gmail.com>
2026-04-26 16:10:37 +0200

    feat: added VirtualServerRmi and VirtualViewRmi
    Implemented the interfaces needed for the RMI technology

---
+++ src/main/java/it/polimi/ingsw/am48/network/VirtualServerRmi.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.am48.network;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/*
+* Estende Remote per (obbligo di) RMI e VirtualServer.
+* Stessi metodi overridati per lanciare RemoteException (per RMI).
+* Aggiunto il metodo connect, necessario per RMI.
+*/
+public interface VirtualServerRmi extends Remote, VirtualServer{
+    void connect(String nickname, VirtualViewRmi client) throws RemoteException;
+    @Override void joinGame(int numPlayers, String nickname) throws RemoteException;
+    @Override void placeTotem(String nickname, char position) throws RemoteException;
+    @Override void takeCard(String nickname, String cardId) throws RemoteException;
+}
