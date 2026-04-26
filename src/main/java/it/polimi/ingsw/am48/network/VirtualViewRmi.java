5eab96fe6c8d47eee555ddec4eae74f0891803da
Pietro Casagrande <pitcasa04.pc@gmail.com>
2026-04-26 17:29:46 +0200

    feat: updated ServerMain for RMI connection initialization
    Commited the fix of a bunch of little things from other classes before pr

--- src/main/java/it/polimi/ingsw/am48/network/VirtualViewRmi.java
+++ src/main/java/it/polimi/ingsw/am48/network/VirtualViewRmi.java
@@ -6,9 +6,7 @@ import it.polimi.ingsw.am48.model.snapshot.GameSnapshot;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/*
-* Stessa logica di VirtualServerRmi, estende Remote e l'interfaccia technology-agnostic.
-*/
+// Stessa logica di VirtualServerRmi, estende Remote e l'interfaccia technology-agnostic.
 public interface VirtualViewRmi extends Remote, VirtualView{
     @Override void showGameDelta(GameDelta delta) throws RemoteException;
     @Override void showInitialSnapshot(GameSnapshot snapshot) throws RemoteException;
