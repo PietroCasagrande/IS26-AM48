171764a3586acbe185e1785f56e3cc182563a1ee
Pietro Casagrande <pitcasa04.pc@gmail.com>
2026-04-27 18:02:44 +0200

    feat: added Server Notification and subclasses, updated SocketServerHandler, pt2 (I committed packages before)

--- src/main/java/it/polimi/ingsw/am48/network/messages/notifications/ErrorNotification.java
+++ src/main/java/it/polimi/ingsw/am48/network/messages/notifications/ErrorNotification.java
@@ -1,10 +1,23 @@
 package it.polimi.ingsw.am48.network.messages.notifications;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import it.polimi.ingsw.am48.network.client.ClientModel;
 
-public class ErrorNotification extends ServerNotification{
+public class ErrorNotification extends ServerNotification {
+    private String message;
+
+    @JsonCreator
+    public ErrorNotification(@JsonProperty("error") String message){
+        this.message = message;
+    }
+
     @Override
     public void apply(ClientModel model) {
+        model.notifyError(message);
+    }
 
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/it/polimi/ingsw/am48/network/messages/notifications/GameDeltaNotification.java
+++ src/main/java/it/polimi/ingsw/am48/network/messages/notifications/GameDeltaNotification.java
@@ -1,10 +1,24 @@
 package it.polimi.ingsw.am48.network.messages.notifications;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import it.polimi.ingsw.am48.model.delta.GameDelta;
 import it.polimi.ingsw.am48.network.client.ClientModel;
 
-public class GameDeltaNotification extends ServerNotification{
+public class GameDeltaNotification extends ServerNotification {
+    private GameDelta delta;
+
+    @JsonCreator
+    public GameDeltaNotification(@JsonProperty("delta") GameDelta delta){
+        this.delta = delta;
+    }
+
     @Override
     public void apply(ClientModel model) {
+        model.applyDelta(delta);
+    }
 
+    public GameDelta getDelta() {
+        return delta;
     }
 }
