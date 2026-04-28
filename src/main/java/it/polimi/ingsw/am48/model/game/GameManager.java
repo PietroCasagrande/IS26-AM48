8871f9060d1ea7552d30ce04f51bd92043ab5e3a
Pietro Casagrande <pitcasa04.pc@gmail.com>
2026-04-28 11:32:00 +0200

    fix: merge conflict in joinGame method of GameManager

--- src/main/java/it/polimi/ingsw/am48/model/game/GameManager.java
+++ src/main/java/it/polimi/ingsw/am48/model/game/GameManager.java
@@ -34,6 +34,9 @@ public class GameManager implements ModelInterface{
     @Override
     public JoinResult joinGame(int numPlayers, String nickname){
         synchronized (this) {
+            if(numPlayers < 2 || numPlayers > 5) {
+                throw new IllegalArgumentException("Invalid number of players: must be between 2 and 5");
+            }
             if(playerToGame.containsKey(nickname)){
                 throw new InvalidActionException("Nickname già in uso: " + nickname);
             }
