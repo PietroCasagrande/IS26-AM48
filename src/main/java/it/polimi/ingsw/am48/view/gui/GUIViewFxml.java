50ad1a4d9c5788a99b62c95a3675005b8fd5346d
Luca M. Brotto <luca@brotto.org>
2026-05-07 16:21:32 +0200

    feat: implement game-board layout, controller and assets

--- src/main/java/it/polimi/ingsw/am48/view/gui/GUIViewFxml.java
+++ src/main/java/it/polimi/ingsw/am48/view/gui/GUIViewFxml.java
@@ -1,3 +1,4 @@
+
 package it.polimi.ingsw.am48.view.gui;
 
 import it.polimi.ingsw.am48.network.VirtualServer;
@@ -5,11 +6,11 @@ import it.polimi.ingsw.am48.network.VirtualServerRmi;
 import it.polimi.ingsw.am48.network.client.ClientModel;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Classe principale per il lancio dell'interfaccia grafica FXML.
+ */
 public class GUIViewFxml extends Application {
     private static VirtualServer server;
     private static ClientModel model;
@@ -24,19 +25,35 @@ public class GUIViewFxml extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        // Inizializzazione dello SceneManager con i riferimenti necessari [cite: 2]
         SceneManager.setup(stage, server, model);
 
-        SplashScreenController controller = (SplashScreenController) SceneManager.changeScene("splash-screen.fxml");
-        if (controller != null) {
-            controller.setServer(server);
-            controller.setModel(model);
+        // Se il model è null (test locale), viene creato un ClientModel di test
+        if (model == null) {
+            System.out.println("Attenzione: model null, inizializzo un ClientModel di test.");
+            model = new ClientModel();
+        }
+
+        // Caricamento della scena della board di gioco [cite: 2]
+        // Ritorna il controller associato al file FXML [cite: 2]
+        Object controller = SceneManager.changeScene("game-board.fxml");
+
+        if (controller instanceof GameBoardController gameBoardController) {
+            // Iniezione delle dipendenze nel controller della board 
+            gameBoardController.setServer(server);
+            gameBoardController.setModel(model);
+        } else {
+            System.err.println("Errore: Il controller caricato non è GameBoardController!");
         }
 
         stage.setTitle("MESOS");
+
+        // Gestione della chiusura pulita dell'applicazione
         stage.setOnCloseRequest(e -> {
             Platform.exit();
             System.exit(0);
         });
+
         stage.show();
     }
 }
