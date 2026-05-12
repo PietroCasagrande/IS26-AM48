62cb65ceae33143e80c5afeb41df90be020d2371
Pietro Casagrande <pitcasa04.pc@gmail.com>
2026-05-13 01:09:38 +0200

    feat: wire MySqlLeaderboardRepository in ServerMain and fix LocalDateTime serialization on Socket

---
+++ src/main/java/it/polimi/ingsw/am48/utils/JsonMapper.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.am48.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonMapper {
+    private static final ObjectMapper INSTANCE = createMapper();
+
+    private static ObjectMapper createMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    public static ObjectMapper get() { return INSTANCE; }
+}
