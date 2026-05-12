08c7c761b8abc67caded62b1022b4acf646f8848
Pietro Casagrande <pitcasa04.pc@gmail.com>
2026-05-13 01:09:00 +0200

    feat: add leaderboard field to ClientGameState and fix Jackson ObjectMapper in SocketServerHandler

--- src/main/java/it/polimi/ingsw/am48/network/client/SocketServerHandler.java
+++ src/main/java/it/polimi/ingsw/am48/network/client/SocketServerHandler.java
@@ -10,6 +10,7 @@ import it.polimi.ingsw.am48.network.messages.commands.JoinGameCommand;
 import it.polimi.ingsw.am48.network.messages.commands.PlaceTotemCommand;
 import it.polimi.ingsw.am48.network.messages.commands.TakeCardCommand;
 import it.polimi.ingsw.am48.network.messages.notifications.ServerNotification;
+import it.polimi.ingsw.am48.utils.JsonMapper;
 
 import java.io.*;
 import java.net.Socket;
@@ -25,7 +26,7 @@ public class SocketServerHandler implements Runnable, VirtualServerSocket {
         this.socket = new Socket(host, port);
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new PrintWriter(socket.getOutputStream(), true);
-        this.mapper = new ObjectMapper();
+        this.mapper = JsonMapper.get();
         this.model = model;
     }
 
@@ -64,6 +65,7 @@ public class SocketServerHandler implements Runnable, VirtualServerSocket {
         try {
             out.println(mapper.writeValueAsString(command));
         } catch (Exception e) {
+            System.err.println("SEND ERROR: " + e.getMessage());
             e.printStackTrace();
         }
     }
